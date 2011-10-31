1139848f31e1e8502c7a492dfcee0f4bd3d9cd8a
David Reed <dreed@thoughtworks.com>
2011-10-31 23:01:19 +0800

    Looking for place to enter programmer names

--- python/django/func_tests/tests/pair_stairs_tests.py
+++ python/django/func_tests/tests/pair_stairs_tests.py
@@ -1,5 +1,6 @@
 from unittest.case import TestCase
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class TestPairStairs(TestCase):
     def test_should_create_pair_stairs_from_list_of_names(self):
@@ -8,6 +9,7 @@ class TestPairStairs(TestCase):
         self.driver.get('http://localhost:8000/create')
         self.assertEqual(self.driver.title, "Create a new pair stair")
         # Enter Names
+        self.driver.find_element(By.CSS_SELECTOR, '#programmer_names')
         # Submit
         # Assert Pair Stairs is displayed
         raise NotImplementedError("You still need to write this!")
