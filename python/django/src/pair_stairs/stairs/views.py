8dadfd9bcd357401d5bc76a0c6393e57c8394e54
David Reed <dreed@thoughtworks.com>
2011-10-31 22:53:45 +0800

    Adding initial template for create stairs

--- python/django/src/pair_stairs/stairs/views.py
+++ python/django/src/pair_stairs/stairs/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 def create_stairs(request):
-    return HttpResponse('We need to do something more!')
+    return render_to_response('create_stairs.html', RequestContext(request))
