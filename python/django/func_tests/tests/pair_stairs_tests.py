7545f792a1315ad7df210e47597248602d918ec4
David Reed <dreed@thoughtworks.com>
2011-10-31 17:10:27 +0800

    Creating first functional test

---
+++ python/django/func_tests/tests/pair_stairs_tests.py
@@ -0,0 +1,9 @@
+from unittest.case import TestCase
+
+class TestPairStairs(TestCase):
+    def test_should_create_pair_stairs_from_list_of_names(self):
+        # Go to page
+        # Enter Names
+        # Submit
+        # Assert Pair Stairs is displayed
+        raise NotImplementedError("You still need to write this!")
