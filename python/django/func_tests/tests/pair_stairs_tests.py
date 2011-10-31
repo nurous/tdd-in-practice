1d81095c3889cdcec3092ada52de3bbd6fc4d36f
David Reed <dreed@thoughtworks.com>
2011-10-31 17:21:31 +0800

    Making the functional test fail for (the first) valid reason

--- python/django/func_tests/tests/pair_stairs_tests.py
+++ python/django/func_tests/tests/pair_stairs_tests.py
@@ -1,9 +1,18 @@
 from unittest.case import TestCase
+from selenium import webdriver
 
 class TestPairStairs(TestCase):
     def test_should_create_pair_stairs_from_list_of_names(self):
         # Go to page
+        self.driver = webdriver.Chrome()
+        self.driver.get('http://localhost:8000/create')
+        self.assertEqual(self.driver.title, "Create a new pair stair")
         # Enter Names
         # Submit
         # Assert Pair Stairs is displayed
         raise NotImplementedError("You still need to write this!")
+
+    def tearDown(self):
+        self.driver.close()
+
+
