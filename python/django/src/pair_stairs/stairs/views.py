8c615beee2471c10144c7f1cf7ec1e40c24ac64e
David Reed <dreed@thoughtworks.com>
2011-11-01 00:02:11 +0800

    Saving programmers to the database

--- python/django/src/pair_stairs/stairs/views.py
+++ python/django/src/pair_stairs/stairs/views.py
@@ -2,11 +2,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
+from pair_stairs.stairs.models import Programmer
 
 def view_stairs(request):
     return render_to_response('stairs.html', RequestContext(request))
 
 def create_stairs(request):
     if request.method == 'POST':
+        names = request.POST['programmer_names']
+        Programmer(name = names).save()
         return redirect(view_stairs)
     return render_to_response('create_stairs.html', RequestContext(request))
