2f9fbbbefd4b34b8328c39a888e17525e7b9da0f
David Reed <dreed@thoughtworks.com>
2011-11-01 21:18:26 +0800

    Adding multiple names to unit tests

--- python/django/src/pair_stairs/stairs/tests.py
+++ python/django/src/pair_stairs/stairs/tests.py
@@ -9,7 +9,11 @@ from django.test import TestCase
 from django.test.client import Client
 from pair_stairs.stairs.models import Programmer
 
-MICKEY_MOUSE = 'Mickey Mouse'
+NAMES = ['Mickey Mouse', 'Minnie Mouse']
+
+def format_names(names):
+    return ', '.join(names)
+
 
 class TestCreatePairStairs(TestCase):
     def test_should_render_page_to_create_new_pair_stairs(self):
@@ -19,19 +23,21 @@ class TestCreatePairStairs(TestCase):
         self.assertTemplateUsed(response, 'create_stairs.html')
 
     def test_should_redirect_to_stairs_after_submission(self):
-        response = Client().post('/create/', {'programmer_names': MICKEY_MOUSE})
+        response = Client().post('/create/', {'programmer_names': format_names(NAMES)})
 
         self.assertRedirects(response, '/stairs/')
 
     def test_should_pass_programmers_to_stairs_after_submission(self):
-        response = Client().post('/create/', {'programmer_names': MICKEY_MOUSE}, follow=True)
+        response = Client().post('/create/', {'programmer_names': format_names(NAMES)}, follow=True)
 
-        self.assertContains(response, MICKEY_MOUSE)
+        for name in NAMES:
+            self.assertContains(response, name)
 
     def test_should_save_programmers(self):
-        Client().post('/create/', {'programmer_names': MICKEY_MOUSE})
+        Client().post('/create/', {'programmer_names': format_names(NAMES)})
 
-        self.assertEquals(Programmer.objects.filter(name = MICKEY_MOUSE).count(), 1)
+        for name in NAMES:
+            self.assertEquals(Programmer.objects.filter(name = name).count(), 1, "Programmer '%s' not found" % name)
 
     def test_should_render_pair_stairs(self):
         response = Client().get('/stairs/')
